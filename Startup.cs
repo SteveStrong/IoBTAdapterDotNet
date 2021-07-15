ee8d233ef088470799e13cc077c0bbcc7f0510bc
Steve <stephen.r.strong@saic.com>
2021-07-15 12:34:36 -0400

    with grpc

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,8 @@ namespace IoBTAdapterDotNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGrpc();
+
             services.AddControllers();
 
             services.AddSingleton<IMedusaEntity, MedusaEntity>();

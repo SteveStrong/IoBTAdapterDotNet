71b8869e6ad5f95a7e2448c3459f6acdfa6af0b8
Steve <stephen.r.strong@saic.com>
2021-07-16 15:29:45 -0400

    new config

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -5,10 +5,9 @@ using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace IoBTAdapterDotNet.Services
-
+namespace grpcoSteve
 {
-    public class GreeterService : CompleteGreeter.CompleteGreeterBase
+    public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
         public GreeterService(ILogger<GreeterService> logger)
@@ -24,6 +23,4 @@ namespace IoBTAdapterDotNet.Services
             });
         }
     }
-
-
 }
--- Startup.cs
+++ Startup.cs
@@ -89,6 +89,10 @@ namespace IoBTAdapterDotNet
             {
                 endpoints.MapHub<AdapterHub>("/adapterHub");
                 endpoints.MapHub<MedusaHub>("/medusaHub");
+                            
+                // Communication with gRPC endpoints must be made through a gRPC client.
+                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+                //endpoints.MapGrpcService<GreeterService>();
                 endpoints.MapControllers();
             });
         }
